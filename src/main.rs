6b83ebc261c0791fa9421b15db5def5de0b1c2a0
makiri1993 <kireew.martin@icloud.com>
2021-05-04 22:36:07 +0200

    improve printing of summary with additional infos

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use chrono::NaiveDate;
+use chrono::{Datelike, NaiveDate};
 use clap::{App, Arg};
 use log::info;
 
@@ -103,12 +103,35 @@ fn print_subcommand_entries(entries: &[time::Entry]) {
 
 fn print_subcommand_summary(entries: BTreeMap<NaiveDate, i64>) {
     let mut sum_hours = 0.0;
-    println!("{0: >10} | {1: >8} | {2: >8}", "Date", "Hours", "Minutes");
+    println!(
+        "{0: >10} | {1: >9} | {2: >8} | {3: >8}",
+        "Date", "Weekday", "Hours", "Minutes"
+    );
+    let mut week_number = 0;
     entries.iter().for_each(|(key, value)| {
+        let week_number_entry = key.iso_week().week();
+        let weekday = key.weekday();
         let hours = value / 60;
         let minutes = value - hours * 60;
+
         sum_hours += hours as f64 + minutes as f64 / 60.0;
-        println!("{0: <10} | {1: >7}h | {2: >7}m", key, hours, minutes);
+
+        let needs_new_line = if week_number != week_number_entry {
+            format!("\nWeek {}\n", week_number_entry)
+        } else {
+            "".to_string()
+        };
+        // println!("{:?}", week_number.week());
+        println!(
+            "{0}{1: <10} | {2: >9} | {3: >7}h | {4: >7}m",
+            needs_new_line,
+            key,
+            weekday.to_string(),
+            hours,
+            minutes,
+        );
+
+        week_number = week_number_entry;
     });
     println!();
     info!("You should have worked at least {}h", entries.len() * 8);
